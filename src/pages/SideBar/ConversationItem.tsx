db0c63460bcb42bce25f44a98ed8fc5de217b7cd
Henry-Tran-7198 <131451055+Henry-Tran-7198@users.noreply.github.com>
2024-02-21 21:29:33 +0700

    Update ConversationItem.tsx

--- src/pages/SideBar/ConversationItem.tsx
+++ src/pages/SideBar/ConversationItem.tsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import styles from './SideBar.module.css'
 
 export interface IConversationItemProps {
+  name: string;
+  lastMessage: string;
+  timeStamp: string;
 }
 
-export default function ConversationItem ({props}) {
+export default function ConversationItem (props: IConversationItemProps) {
   return (
     <div className={styles.container}>
       <p className={styles.icon}>{props.name[0]}</p>
