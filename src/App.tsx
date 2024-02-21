9e5d6860c9a5386a8bc628a9887a54d5ac19953f
Henry-Tran-7198 <131451055+Henry-Tran-7198@users.noreply.github.com>
2024-02-21 18:37:59 +0700

    SideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { Outlet, createBrowserRouter } from 'react-router-dom'
 import './App.css'
+import SidebarPage from './pages/SideBar/SidebarPage'
+
+const router = createBrowserRouter([
+  {path: "/", element: <Layout/> }
+])
+
+function Layout () {
+  
+}
 
 function App() {
   return (
     <>
-      
+<<<<<<< Updated upstream
+      <SidebarPage/>
+=======
+      <Outlet/>
+>>>>>>> Stashed changes
     </>
   )
 }
---
+++ src/pages/SideBar/SidebarPage.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import ConversationItem from "./ConversationItem";
+
+export interface ISidebarPageProps {}
+
+export default function SidebarPage() {
+    const [conversations, setConversations] = React.useState([
+        {
+            name: "Test#1",
+            lastMessage: "Last Message #1",
+            timeStamp: "today",
+        },
+        {
+            name: "Test#2",
+            lastMessage: "Last Message #2",
+            timeStamp: "today",
+        },
+        {
+            name: "Test#3",
+            lastMessage: "Last Message #3",
+            timeStamp: "today",
+        },
+        {
+            name: "Test#4",
+            lastMessage: "Last Message #4",
+            timeStamp: "today",
+        },
+        {
+            name: "Test#5",
+            lastMessage: "Last Message #5",
+            timeStamp: "today",
+        },
+        {
+            name: "Test#6",
+            lastMessage: "Last Message #6",
+            timeStamp: "today",
+        },
+    ])
+  return (
+    <div>
+      <div>
+        <input placeholder="Find or start a conversation" />
+      </div>
+  
+      <div>Friend</div>
+
+      <div>
+        DIRECT MESSAGE
+        <button>+</button>
+        <div className="sb-conversation">
+            {conversations.map((conversation)=>{
+                return <ConversationItem  props={conversation}/>
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}
