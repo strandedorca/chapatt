3d2f87820b2335b01ea4c0558c7041f1142e91e8
Stranded Orca <bestofmau@gmail.com>
2024-02-21 09:16:19 +0700

    Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { Container, Grid } from '@mui/material'
 import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Container maxWidth={false}>
+      <Grid container spacing={2}>
+        <Grid id='channel-bar' lg={1}>
+          Channels
+        </Grid>
+        <Grid id='left-bar' lg={2}>
+          Channel Details
+        </Grid>
+
+        <Grid container lg={9}>
+          <Grid id="header" lg={12}>
+            Header
+          </Grid>
+          <Grid container lg={12}>
+            <Grid id="conversation" lg={8}>
+              Main
+            </Grid>
+            <Grid id="right-bar" lg={4}>
+              User information
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
