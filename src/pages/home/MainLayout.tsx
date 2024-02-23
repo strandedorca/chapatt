60f0549e8a338544a4a02ff050a9f46380823249
strandedorca <bestofmau@gmail.com>
2024-02-23 20:20:49 +0700

    Fix bugs from SidebarPage

--- src/pages/home/MainLayout.tsx
+++ src/pages/home/MainLayout.tsx
@@ -3,6 +3,7 @@ import ServerSidebar from "./ServerSidebar"
 import Header from "./Header"
 import Chat from "./Chat"
 import { styled } from "@mui/system"
+import SidebarPage from "../sidebar/SidebarPage"
 
 const FHBox = styled(Box)({
   height: '100%',
@@ -17,7 +18,7 @@ const MainLayout = () => {
         width="240px"
         position="fixed"
       >
-        <ServerSidebar />
+        <SidebarPage />
       </FHBox>
 
       {/* Main */}
