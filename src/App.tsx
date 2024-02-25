759be3f1eb8dc7151c19781eea8ac8e0cb4c4168
Henry-Tran-7198 <131451055+Henry-Tran-7198@users.noreply.github.com>
2024-02-25 18:19:37 +0700

    Fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import { Outlet, createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import SidebarPage from './pages/SideBar/SidebarPage'
 
-function Layout () {
-  
-}
-
 function App() {
   return (
     <>
